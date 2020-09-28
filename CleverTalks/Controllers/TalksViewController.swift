613df71f947a1b8261fe666f689a419e4e1c0bd1
Irynavb <irynabetancourt@gmail.com>
2020-09-28 13:29:13 -0400

    Initial commit

--- CleverTalks/Controllers/TalksViewController.swift
+++ CleverTalks/Controllers/TalksViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TalksViewController.swift
 //  CleverTalks
 //
 //  Created by Iryna V Betancourt on 9/28/20.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TalksViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
